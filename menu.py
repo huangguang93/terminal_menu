988d2642dd30d479d9e96a172a0addcd105dc5ef
zhouhuang <zhouhuang@qiyi.com>
2019-07-02 18:32:24 +0800

    优化代码

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import os
+import re
 import sys
 import termios
 import tty
@@ -9,12 +10,15 @@ import tty
 
 class Menu(object):
     def __init__(self, clear_screen=True):
+        if clear_screen:
+            os.system("clear")
+        self._term_start_pos = self.getpos()[0]
+        self._term_end_pos = self._term_start_pos
         self.offset = " " * 8   # 菜单距离左侧偏移量
         self.id_show = True  # 是否显示列表id
-        self.title_show = True  # 标题是否显示
-        self.foot_show = True   # 页脚是否显示
+        self.title = True  # 标题是否显示
+        self.foot = True   # 页脚是否显示
         self.page_size = 10  # 每页显示多少条
-        self.back_code = "back"  # 定义"返回"时应当return的状态码
         self.title_delimiter = " > "  # 定义标题分隔符
         self.pointer = "->"   # 定义选择指示器
         self.title_color = "purple"
@@ -22,11 +26,8 @@ class Menu(object):
         self.body_word_color = ""
         self.body_word_switch_color = "blue"
 
-        if clear_screen:
-            os.system("clear")
-
-    def menu_style(self, offset=8, id_show=True, title_show=True, foot_show=True,
-                   page_size=10, back_code="back", title_delimiter=" > ", pointer="->",
+    def menu_style(self, offset=8, id_show=True, title=True, foot=True,
+                   page_size=10, title_delimiter=" > ", pointer="->",
                    title_color="purple", foot_color="yellow", body_word_color="",
                    body_word_switch_color="blue"):
         """
@@ -34,10 +35,9 @@ class Menu(object):
         供选择的颜色：'black', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white'
         :param offset:
         :param id_show:
-        :param title_show:
-        :param foot_show:
+        :param title:
+        :param foot:
         :param page_size:
-        :param back_code:
         :param title_delimiter:
         :param pointer:
         :param title_color: 'black', 'red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white'
@@ -48,10 +48,9 @@ class Menu(object):
         """
         self.offset = " " * offset   # 菜单距离左侧偏移量
         self.id_show = id_show  # 是否显示列表id
-        self.title_show = title_show  # 标题是否显示
-        self.foot_show = foot_show   # 页脚是否显示
+        self.title = title  # 标题是否显示
+        self.foot = foot   # 页脚是否显示
         self.page_size = page_size  # 每页显示多少条
-        self.back_code = back_code  # 定义"返回"时应当return的状态码
         self.title_delimiter = title_delimiter  # 定义标题分隔符
         self.pointer = pointer   # 定义选择指示器
         self.title_color = title_color   # 标题颜色
@@ -85,33 +84,41 @@ class Menu(object):
         end = '\033[%sm' % styles['default']['end'] if style else ''
         return '%s%s%s' % (style, string, end)
 
-    def clr_scr(self, search=False):
+    def getpos(self):
+        buf = ""
+        stdin = sys.stdin.fileno()
+        tattr = termios.tcgetattr(stdin)
+
+        try:
+            tty.setcbreak(stdin, termios.TCSANOW)
+            sys.stdout.write("\x1b[6n")
+            sys.stdout.flush()
+
+            while True:
+                buf += sys.stdin.read(1)
+                if buf[-1] == "R":
+                    break
+        finally:
+            termios.tcsetattr(stdin, termios.TCSANOW, tattr)
+
+        # reading the actual values, but what if a keystroke appears while reading
+        # from stdin? As dirty work around, getpos() returns if this fails: None
+        try:
+            matches = re.match(r"^\x1b\[(\d*);(\d*)R", buf)
+            groups = matches.groups()
+        except AttributeError:
+            return None
+        return int(groups[0]), int(groups[1])
+
+    def clear_old_content(self):
         """
         清理屏幕
-        用空格覆盖原来的菜单(清空菜单)，再将光标移动到第一行重新开始写入数据
         """
-        if self.title_show and search:
-            back_line = 2 + 3  # 2 因为search 2行    3 对应标题三行
-        elif search:
-            back_line = 2  # 2 因为搜索框2行
-        elif self.title_show and self.foot_show:
-            back_line = self.page_size + 5
-        elif self.title_show:
-            back_line = self.page_size + 3  # 3 因为标题占三行
-        elif self.foot_show:
-            back_line = self.page_size + 2  # 2 因为foot占两行
-        else:
-            back_line = self.page_size
-        # 光标移动到菜单开始位置
-        sys.stdout.write('\033[%dA\033[K' % (back_line,))
-        sys.stdout.flush()
-        # 生成空行覆盖整个菜单
-        s = [" " * 100 for i in range(0, back_line)]
-        sys.stdout.write("\n".join(s))
-        sys.stdout.flush()
-        # 光标移动到菜单开始位置
-        sys.stdout.write('\033[%dA\033[K' % (back_line - 1,))
-        sys.stdout.flush()
+        self._term_end_pos = self.getpos()[0]
+        back_line = self._term_end_pos - self._term_start_pos
+        for i in range(back_line):
+            sys.stdout.write("\r\033[K\033[1A")
+            sys.stdout.flush()
 
     def getch(self):
         """获取键盘输入"""
@@ -127,7 +134,8 @@ class Menu(object):
         return ch
 
     def title_box(self, title):
-        if self.title_show:
+        """ title block"""
+        if self.title:
             title_base = "\n" + self.offset + (len(self.pointer) + 1) * " " + "主菜单"
             if title is not None and isinstance(title, list):
                 title_info = title_base + self.title_delimiter + self.title_delimiter.join(title)
@@ -137,6 +145,7 @@ class Menu(object):
             sys.stdout.flush()
 
     def body_box(self, pos, choose, start):
+        """ body block """
         i = 0
         s = ""
         while i < self.page_size:
@@ -159,11 +168,11 @@ class Menu(object):
         sys.stdout.flush()
 
     def foot_box(self, total, start, page):
-        # 菜单页脚显示
+        """  foot block """
         s = ""
         next_page = False
         prev_page = False
-        if self.foot_show:
+        if self.foot:
             if start + self.page_size < total:
                 next_page = True
             if start - self.page_size >= 0:
@@ -182,6 +191,7 @@ class Menu(object):
         sys.stdout.flush()
 
     def search_box(self, choose, title):
+        self.clear_old_content()
         search_title = ["搜索"] if title is None else title + ["搜索"]
         self.title_box(search_title)
         s = "\r\n" + self.offset + "关键字: "
@@ -189,97 +199,79 @@ class Menu(object):
         sys.stdout.flush()
         keyword = sys.stdin.readline().strip()
         filter_choose = list(filter(lambda x: keyword in str(x[1]), choose))
-        self.clr_scr(search=True)
         return filter_choose
 
-    def controller(self, page_size, page, pos, start, total):
-        search = False
-        end = False
-        back = False
-        id = 0
-        key = self.getch()
-        if key == "\r":  # enter
-            if total != 0:
-                id = page_size * (page - 1) + pos
-                end = True
+    def menu_box(self, choose, pos, page_data, page, start, title=None):
+        self.clear_old_content()
+        total = len(choose)
+        self.title_box(title)
+        self.body_box(pos, page_data, start)
+        self.foot_box(total, start, page)
 
-        elif key in ["\x1b[A", "k", "K"]:
-            pos -= 1
-
-        elif key in ["\x1b[B", "j", "J"]:
-            pos += 1
-
-        elif key in ["\x1b[D", "h", "H"]:  # 上一页
-            if start - page_size >= 0:
-                pos = 0
-                start = start - page_size
-                page -= 1
-
-        elif key in ["\x1b[C", "l", "L"]:  # 下一页
-            if start + page_size < total:
-                pos = 0
-                start = start + page_size
-                page += 1
-        elif key in ["b", "B"]:  # 返回
-            back = True
-
-        elif key in ["S", "s"]:  # 搜索
-            search = True
-
-        return pos, page, start, search, end, id, back
-
-    def menu_box(self, choose, title=None):
+    def menu(self, choose, title=None):
         pos = 0
         start = 0
         page = 1
         page_size = self.page_size
-        total = len(choose)
+
+        if title and isinstance(title, list) is False:
+            title = [str(title)]
+        choose_with_id = [[i, v] for i, v in enumerate(choose)]
+        choose_list = choose_with_id
+
         while True:
+            total = len(choose_list)
             if start + page_size < total:
-                page_list = choose[start:start + page_size]
+                page_list = choose_list[start:start + page_size]
             else:
-                page_list = choose[start:total]
+                page_list = choose_list[start:total]
+
             # 控制指针到达边界时
             if pos < 0:
                 pos = len(page_list) - 1
             elif pos >= len(page_list):
                 pos = 0
 
-            self.title_box(title)
-            self.body_box(pos, page_list, start)
-            self.foot_box(total, start, page)
+            self.menu_box(choose_list, pos,  page_list, page, start, title)
 
-            pos, page, start, search, end, id, back = self.controller(
-                page_size=page_size, page=page, pos=pos, start=start, total=total)
+            key = self.getch()
+            if key == "\r":  # enter
+                if len(choose_list) != 0:
+                    id = page_size * (page - 1) + pos
+                    return choose_list[id][0], choose_list[id][1]
 
-            if end:
-                return 0, id
-            self.clr_scr()
-            if search:
-                return 1, None
-            elif back:
-                return 2, None
+            elif key in ["b", "B"]:  # 返回
+                return -1, None
 
-    def menu(self, choose, title=None):
-        if title and isinstance(title, list) is False:
-            title = [str(title)]
-        # 给每项添加id
-        choose_with_id = [[i, v] for i, v in enumerate(choose)]
-        choose_list = choose_with_id
-        while True:
-            action, id = self.menu_box(choose_list,  title)
-            if action == 0:
-                return choose_list[id][0], choose_list[id][1]
-            elif action == 1:
+            elif key in ["\x1b[A", "k", "K"]:
+                pos -= 1
+
+            elif key in ["\x1b[B", "j", "J"]:
+                pos += 1
+
+            elif key in ["\x1b[D", "h", "H"]:  # 上一页
+                if start - page_size >= 0:
+                    pos = 0
+                    start = start - page_size
+                    page -= 1
+
+            elif key in ["\x1b[C", "l", "L"]:  # 下一页
+                if start + page_size < total:
+                    pos = 0
+                    start = start + page_size
+                    page += 1
+
+            elif key in ["S", "s"]:  # 搜索
                 choose_list = self.search_box(choose_list, title)
-            elif action == 2:
+
+            elif key in ["x", "x"]:  # 取消搜索
                 choose_list = choose_with_id
 
 
 if __name__ == '__main__':
     test_data = ["阿里巴巴", "百度", "腾讯", "今日头条", "爱奇艺", "美团", "饿了吗", "小米",
                  "支付宝", "京东", "拼多多", "微博", "携程", "网易", "哔哩哔哩", "迅雷", "360"]
-    m = Menu()
-    m.menu_style(page_size=10)
+    m = Menu(clear_screen=False)
+    m.menu_style(page_size=10, title=False)
     pos = m.menu(test_data, title="互联网公司")
     print("Your word is ", pos)
